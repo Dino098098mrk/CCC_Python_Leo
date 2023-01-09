83f38233bd92bbb6d074b1b9b1d9408e8f534c49
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-09 02:00:48 +0000

    CCC

---
+++ CCC2006.py/J2.py
@@ -0,0 +1,12 @@
+x = int(input("Enter m: "))
+y = int(input("Enter n: "))
+
+counter = 0
+
+
+for i in range(1,x+1):
+  for j in range(1,y+1):
+    if i + j == 10:
+      counter += 1
+
+print(counter)
