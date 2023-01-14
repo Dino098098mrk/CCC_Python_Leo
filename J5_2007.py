4edd060e435b7cd20119afced94f18ef5c5bf51c
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-14 19:42:03 +0000

    CCC

--- J5_2007.py
+++ J5_2007.py
@@ -1,12 +1,13 @@
+#DP
 distance  =[1, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000]
 ways = [0]*7000
+ways[0] = 1
 min = int(input())
 max = int(input())
 add = int(input())
 # if add != 0:
 #   stop = int(input())
 # else:
-ways = 0
 
 for i in range(add):
   hotel= int(input())
@@ -18,3 +19,15 @@ for x in distance:
   x -= 1
   if x + max >= 7000:
     for i in range(min,7000 -x):
+      if ways[x] == 0:
+        continue
+      if x + i+1 in distance:
+        ways[x+i] += ways[x]
+  else:
+    for i in range(min,max+1):
+      if ways[x] == 0:
+        continue
+      if x + i+1 in distance:
+        ways[x+i] += ways[x]
+
+print(ways[-1])
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # import MockExamSol
 
 # import CCC2007
-import J4_2006
+# import J4_2006
+import J5_2007
