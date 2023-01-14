87199e6cfdd07ecfafe6e03534a522ba34039cba
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-14 19:31:21 +0000

    CCC

--- J5_2007.py
+++ J5_2007.py
@@ -1,19 +1,20 @@
-distance  =[0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000]
-
+distance  =[1, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000]
+ways = [0]*7000
 min = int(input())
 max = int(input())
 add = int(input())
 # if add != 0:
 #   stop = int(input())
 # else:
+ways = 0
 
 for i in range(add):
   hotel= int(input())
   distance.append(hotel)
   distance.sort()
-  
-ways = 0
 
-if max < distance[1]:
-  ways = 0
 
+for x in distance:
+  x -= 1
+  if x + max >= 7000:
+    for i in range(min,7000 -x):
