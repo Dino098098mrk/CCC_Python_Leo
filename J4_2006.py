821eca51239d0b29016a4bd854c12a99ff883bcb
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-14 19:01:54 +0000

    CCC

--- J4_2006.py
+++ J4_2006.py
@@ -1,4 +1,4 @@
-from 
+from queue import PriorityQueue
 array = [[0 for x in range(8)]for x in range(8)] 
 constraint = [0]*8
 
@@ -8,7 +8,7 @@ array[2][1] = 1
 array[3][4] = 1
 array[3][5] = 1
 
-print(array)
+# print(array)
 
 
 while(True):
@@ -26,6 +26,21 @@ for i in array:
         constraint[j] +=1
 
 print(constraint)
-      
+
+pq = PriorityQueue() #min heap
+output = []
+for i in range(1,8):
+  if constraint[i] == 0:
+    pq.put(i)
+
+while not pq.empty():
+  task =pq.get()
+  output.append(task)
+  for i in range(1,8):
+    if array[task][i]==1:
+      constraint[i] -=1
+      if constraint[i]==0:
+        pq.put(i)
+        
         
 
