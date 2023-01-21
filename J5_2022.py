3f4e8dbaa1d4882c5dd6f9ab9c1c0729bfec9c8d
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-21 18:37:36 +0000

    CCC

--- J5_2022.py
+++ J5_2022.py
@@ -22,4 +22,15 @@ for i in range(totalTrees):
   for j in range(totalTrees):
     if i==j and i != treeCount-1:
       continue
-    
+    verticalBound = treeList[i][0]
+    horizontalBound = treeList[j][1]
+    currentSize = extremeSize
+    for k in range(totalTrees):
+      if k ==i or k==j:
+        continue
+      if treeList[k][0]>verticalBound and treeList[k][1]>horizontalBound:
+        verticalWindowSize = treeList[k][0] - verticalBound - 1
+        horizontalWindowSize = treeList[k][1] - horizontalBound-1
+        currentSize = min(currentSize, max(verticalWindowSize, horizontalWindowSize))
+      if(min(numberOfGrid-verticalBound, numberOfGrid-horizontalBound-1)<currentSize):
+        currentSize = min(numberOfGrid-verticalBound, numberOfGrid-horizontalBound-1)
