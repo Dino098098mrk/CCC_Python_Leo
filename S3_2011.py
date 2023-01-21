d5eb60eed7d1a40bc5ca6c45bf0c8226eac19933
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-21 19:25:27 +0000

    CCC

--- S3_2011.py
+++ S3_2011.py
@@ -7,8 +7,15 @@ def isCrystal(magLevel, x,y):
 
   zoomout_x = x 
   zoomout_y = y
-
-
+  for i in range(magLevel-1):
+    zoomout_x //= 5
+    zoomout_y //= 5
+  if(zoomout_x, zoomout_y) in {(1,0),(2,0),(3,0),(2,1)}:
+    return True
+  elif(zoomout_x, zoomout_y) in {(1,1),(2,2),(3,1)}:
+    return isCrystal(magLevel-1,x%(5**(magLevel-1)),y%(5**(magLevel-1)))
+  
+    
 CountT = int(input())
 testCases = []
 for i in range(CountT): #take away: map()
