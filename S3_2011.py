3280ca828256cdb845b00d7729ce1af9a485f158
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-21 19:20:25 +0000

    CCC

--- S3_2011.py
+++ S3_2011.py
@@ -1,7 +1,21 @@
+def isCrystal(magLevel, x,y):
+  if magLevel == 1:
+    if (x,y) in {(1,0),(2,0),(3,0),(2,1)}:
+      return True
+    else:
+      return False
+
+  zoomout_x = x 
+  zoomout_y = y
+
+
 CountT = int(input())
-cases = []
+testCases = []
 for i in range(CountT): #take away: map()
- cases.append(list(map(int,input().split())))
+ testCases.append(list(map(int,input().split()))) # adds value to the list 
+for case in testCases:
+  if isCrystal(case[0], case[1], case[2]):
+    print("crystal")
+  else:
+    print("empty")
 
-
-print(cases)
