9664e4921fabf074e5b4bf279c535207731a61e9
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-22 22:16:12 +0000

    CCC

---
+++ J1_2018.py
@@ -0,0 +1,14 @@
+first = int(input())
+second = int(input())
+third = int(input())
+fourth = int(input())
+
+#if first equal to 8 or 9 and if second and third are the same  and fourth equal to 8 or 9
+if first == 8 or first == 9:
+  if second == third:
+    if fourth == 8 or fourth == 9:
+      print("ignore")
+    else:
+      print("answer")
+  
+  
--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 
 # import CCC2007
 # import J4_2006
-import S3_2018
+import J1_2018
