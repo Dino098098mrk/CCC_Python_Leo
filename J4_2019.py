7b969141dcdd514b8492a0c51db93a3774d11ddb
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-26 04:41:04 +0000

    CCC

---
+++ J4_2019.py
@@ -0,0 +1,32 @@
+grid = [[1,2],[3,4]]
+
+x = input();
+
+#flip grid[0][0] with grid[1][0]
+#flip grid[0][1] with grid[1][1]
+
+for i in range(len(x)):
+  if x[i] == "H":
+    temp = grid[0][0];
+    temp2 = grid[1][0];
+    grid[1][0] = temp
+    grid[0][0] = temp2
+
+    temp3 = grid[0][1];
+    temp4 = grid[1][1];
+    grid[1][1] = temp3
+    grid[0][1] = temp4
+
+  else:
+    temp = grid[0][0];
+    temp2 = grid[0][1];
+    grid[0][0] = temp2
+    grid[0][1] = temp
+
+    temp3 = grid[1][0];
+    temp4 = grid[1][1];
+    grid[1][0] = temp3
+    grid[1][1] = temp4
+
+    
+print(grid)
--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 
 # import CCC2007
 # import J4_2006
-import J3_2018
+import J4_2019
