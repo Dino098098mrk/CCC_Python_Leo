41cf7d58364f6ce0624c66b46f82ceaefa1d27ab
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-28 18:16:52 +0000

    CCC

--- S3_2018.py
+++ S3_2018.py
@@ -16,10 +16,40 @@ for i in range(x):
     if start_col!= -1:
       startPosition = [i, start_col]
   factoryMap.append(list(row))
-  conceptualmap[::-1]*y
+  conceptualmap.append([-1]*y)
   
-for i in range(x-1):
-  for j in range(y-1):
+for i in range(x):
+  for j in range(y):
+    if factoryMap[i][j]=='C':
+      #check down
+      count = 0
+      while True:
+        if factoryMap[i+count][j] in ['.','S']:
+          factoryMap[i+count][j] = '*'
+        elif factoryMap[i+count][j]=='W':
+          break
+        count+=1
+
+      #check up
+      count = 0
+      while True:
+        if factoryMap[i-count][j] in ['.','S']:
+          factoryMap[i-count][j] = '*'
+        elif factoryMap[i-count][j]=='W':
+          break
+        count+=1
+
+            #check up
+      count = 0
+      while True:
+        if factoryMap[i-count][j] in ['.','S']:
+          factoryMap[i-count][j] = '*'
+        elif factoryMap[i-count][j]=='W':
+          break
+        count+=1
+
+      
+    
     #if factoryMap[i][j] == "C" then check up/down/left/right in factoryMap update factory map spot to *
     if factoryMap[i][j] == "C":
       factoryMap[i][j+1] = "*"
