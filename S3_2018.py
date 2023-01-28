51be3e25c215dba428fee19992fe9c3af91f0568
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-28 18:37:08 +0000

    CCC

--- S3_2018.py
+++ S3_2018.py
@@ -66,7 +66,8 @@ while len(queue) != 0: #2. while loop for BFS
   currentCell = queue.pop(0) #3. extract the first item
   r = currentCell[0]
   c = currentCell[1]
-  #4. check the cell
+  #4. check the cell and append the children
+  #checkup
   if (conceptualmap[r-1][c]==-1 \
       or (conceptualmap[r][c]<conceptualmap[r-1][c] and factoryMap[r][c]=='U')\
       or conceptualmap[r][c]+1<conceptualmap[r-1][c] and factoryMap[r-1][c] not in ['W','*'] and factoryMap[r][c] not in ['D', 'L', 'R']):
@@ -75,6 +76,33 @@ while len(queue) != 0: #2. while loop for BFS
       else: #case when we are on a conveyer
         conceptualmap[r-1][c]=conceptualmap[r][c]
       queue.append([r-1,c])
+  #check down
+  if (conceptualmap[r+1][c]==-1 \
+      or (conceptualmap[r][c]<conceptualmap[r-1][c] and factoryMap[r][c]=='D')\
+      or conceptualmap[r][c]+1<conceptualmap[r-1][c] and factoryMap[r-1][c] not in ['W','*'] and factoryMap[r][c] not in ['U', 'L', 'R']):
+      if factoryMap[r][c] not in ['U', 'D', 'L', 'R']:
+        conceptualmap[r+1][c]= conceptualmap[r][c]+1
+      else: #case when we are on a conveyer
+        conceptualmap[r+1][c]=conceptualmap[r][c]
+      queue.append([r+1,c])
+  #check left
+  if (conceptualmap[r][c+1]==-1 \
+      or (conceptualmap[r][c]<conceptualmap[r-1][c] and factoryMap[r][c]=='L')\
+      or conceptualmap[r][c]+1<conceptualmap[r-1][c] and factoryMap[r-1][c] not in ['W','*'] and factoryMap[r][c] not in ['D', 'U', 'R']):
+      if factoryMap[r][c] not in ['U', 'D', 'L', 'R']:
+        conceptualmap[r][c+1]= conceptualmap[r][c]+1
+      else: #case when we are on a conveyer
+        conceptualmap[r][c+1]=conceptualmap[r][c]
+      queue.append([r,c+1])
+  #check right
+  if (conceptualmap[r][c-1]==-1 \
+      or (conceptualmap[r][c]<conceptualmap[r-1][c] and factoryMap[r][c]=='R')\
+      or conceptualmap[r][c]+1<conceptualmap[r-1][c] and factoryMap[r-1][c] not in ['W','*'] and factoryMap[r][c] not in ['D', 'L', 'U']):
+      if factoryMap[r][c] not in ['U', 'D', 'L', 'R']:
+        conceptualmap[r][c-1]= conceptualmap[r][c]+1
+      else: #case when we are on a conveyer
+        conceptualmap[r][c-1]=conceptualmap[r][c]
+      queue.append([r,c-1])
 
     
     
