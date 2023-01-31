e549a4a65f58ed8efae3d170f4dd96f8370abf65
Fishybear <77943019+FishinBear@users.noreply.github.com>
2023-01-31 06:18:27 +0000

    CCC

--- S1_2020.py
+++ S1_2020.py
@@ -17,8 +17,20 @@ for i in range(len(records)):
     bottom = abs(int(records[i][0]) - int(records[i+1][0]))
     speeds.append((top/bottom))
 # you have to test every single possible combination 
+
+
+for i in records:
+  for j in records[1:]:
+    if i[0] == j[0] and i[1] == j[1]:
+      continue
+    else:
+      
+      speeds.append(abs( int(i[1])  - int( j[1]))/ abs(int(i[0]) - int(j[0])))
     
+
 print(max(speeds))
+
+# print(max(speeds))
   # get speed 
 
   # |X − C|/C
