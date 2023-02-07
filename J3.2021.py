a36d3a91f8fb8aa58d7f78dfbea7fe49a22a583f
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-07 04:38:12 +0000

    CCC

--- J3.2021.py
+++ J3.2021.py
@@ -1,8 +1,11 @@
 output = ""
 while(True):
+  last = 
   instruction =input()
 
   if int(instruction) == 99999:
     break
   else:
+    if (int(instruction[0]) + int(instruction[1])) % 2 == 0:
+      output += "right"
     
