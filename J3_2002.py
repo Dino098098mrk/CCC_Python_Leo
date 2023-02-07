b3c6873b6ebd3a567fdd836069f13e4b774266e8
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-07 04:16:06 +0000

    CCC

--- J3_2002.py
+++ J3_2002.py
@@ -6,8 +6,7 @@ red = int(input("Cost of RED tickets:"))
 orange = int(input("Cost of ORANGE tickets:"))
 limit = int(input("How much must be raised with ticket sales?"))
 
-#write a function that takes pink, green, red and orange tickets and returns all possible combination to make limit
-def ticket_combinations(pink, green, red, orange):
-  return com(pink, green, red, orange)
 
-print(ticket_combinations(pink, green, red, orange))
+#write a function
+  
+
