a657b6a4087001cc3292e9b180421a3562b161d6
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-07 04:33:29 +0000

    CCC

--- J3_2002.py
+++ J3_2002.py
@@ -1,4 +1,4 @@
-from itertools import combinations as com
+from itertools import combinations
 
 pink = int(input("Cost of PINK tickets:"))
 green = int(input("Cost of GREEN tickets:"))
@@ -7,6 +7,4 @@ orange = int(input("Cost of ORANGE tickets:"))
 limit = int(input("How much must be raised with ticket sales?"))
 
 
-#write a function
-  
 
