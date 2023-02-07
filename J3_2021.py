2b6a71e6484aafa5c4b5ca1da1eb663ad9136488
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-07 04:49:57 +0000

    CCC

--- J3_2021.py
+++ J3_2021.py
@@ -6,10 +6,11 @@ while(True):
   if int(instruction) == 99999:
     break
   else:
-    if (int(instruction[0]) + int(instruction[1])) % 2 == 0:
+    if (((int(instruction[0]) + int(instruction[1])) % 2 == 0) and (int(instruction[0]) + int(instruction[1]) != 0)):
       output += "right " + instruction[2:] +"\n"
       last = "right "
     elif (int(instruction[0]) + int(instruction[1])) % 2 != 0:
+      
       output += "left " + instruction[2:] +"\n"
       last = "left "
     elif (int(instruction[0]) + int(instruction[1])) == 0:
