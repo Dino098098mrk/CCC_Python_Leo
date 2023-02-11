a55c8b7f102a6e350919e841c326cbcdfe3477e7
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-11 18:19:26 +0000

    CC

--- J3_2002.py
+++ J3_2002.py
@@ -1,10 +1,10 @@
 from itertools import combinations
 
-pink = int(input("Cost of PINK tickets:")) 1
-green = int(input("Cost of GREEN tickets:")) 2
-red = int(input("Cost of RED tickets:"))3
-orange = int(input("Cost of ORANGE tickets:"))4 
-limit = int(input("How much must be raised with ticket sales?")) 3
+pink = int(input("Cost of PINK tickets:")) 
+green = int(input("Cost of GREEN tickets:")) 
+red = int(input("Cost of RED tickets:"))
+orange = int(input("Cost of ORANGE tickets:"))
+limit = int(input("How much must be raised with ticket sales?")) 
 
 
 
