a347e927909d11761a471f941345116e8125915f
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-11 19:18:39 +0000

    CCC

--- S2_2006.py
+++ S2_2006.py
@@ -2,8 +2,11 @@ plaintext = str(input())
 encrypted = str(input())
 translate = str(input())
 dict =dict()
+
+#construct dictionary
 for i in range(len(plaintext)):
   if(encrypted[i] not in dict.keys):
     dict[encrypted[i]]=plaintext[i]
 
-
+#translate
+for 
